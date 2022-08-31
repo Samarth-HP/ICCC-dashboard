ec8b1a27f8a9a5062a77138a314c463056b86fa8
Demon <F/22nG;c9@zB4%P>
2022-08-31 16:22:43 +0530

    updated

---
+++ src/components/QuestionWIthIframe.tsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+
+const jwt = require("jsonwebtoken");
+
+const METABASE_SITE_URL = "http://167.71.234.32:3000";
+const METABASE_SECRET_KEY = "68a529116afd75d19c1d625133ea50207a6571d5e786a25a24c14f61555886b5";
+
+const QuestionWithIframe = ({questionId, height, width}: { questionId: any, height?: string, width?: string }) => {
+    const [url, setUrl] = useState('');
+    const generateUrl = () => {
+        const payload = {
+            resource: {question: questionId},
+            params: {},
+            exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+        };
+        const token = jwt.sign(payload, METABASE_SECRET_KEY);
+
+        setUrl(METABASE_SITE_URL + "/embed/question/" + token + "#bordered=false&titled=false&downloadable=true");
+    }
+    useEffect(() => {
+        if (questionId) {
+            generateUrl();
+        }
+    }, [questionId])
+    if (!url) {
+        return <div>
+            Not Authenticated, Please check if embed is enabled for this Object
+        </div>
+    }
+    return <iframe
+        src={url}
+        frameBorder="0"
+        width={width || '100%'}
+        height={height || "300"}
+        allowTransparency
+    />
+}
+export default QuestionWithIframe
