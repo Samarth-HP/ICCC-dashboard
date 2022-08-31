a8d2005c3080bd619b9970d63a64a4d158e63664
Shivansh Rana <shivanshrana789@gmail.com>
2022-08-31 22:55:04 +0530

    nipun lakshya dashboard embed

--- src/pages/EducationDashboard/Student Assessment Performance (Grade 1-3).tsx
+++ src/pages/EducationDashboard/Student Assessment Performance (Grade 1-3).tsx
@@ -446,79 +446,37 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
             </Row>
           </Col>
           <Col span={6}>
-          <Row>
+            <Row>
               <Col span={24}>
                 <div className="NIPUNheading">NIPUN Lakshya Analysis</div>
               </Col>
             </Row>
             <Row>
               <Col span={24} style={{ display: "flex" }}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/4fee588d-5411-41a2-991a-24c02cbbec55"
-                  frameBorder="0"
-                  width="50%"
-                  height="180"
-                  allowTransparency
-                ></iframe>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/9f349098-91f6-49b2-9b11-095f75350009"
-                  frameBorder="0"
-                  width="50%"
-                  height="180"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={133} width="50%" height="200" />
+                <QuestionWithIframe questionId={136} width="50%" height="200" />
               </Col>
               <Col span={24} style={{ display: "flex" }}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/52fa4f11-2197-4306-8107-5e3716ee9d49"
-                  frameBorder="0"
-                  width="50%"
-                  height="180"
-                  allowTransparency
-                ></iframe>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/7ce3d9ca-bc8e-4eff-a962-f9fb50c02f93"
-                  frameBorder="0"
-                  width="50%"
-                  height="180"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={134} width="50%" height="200" />
+                <QuestionWithIframe questionId={135} width="50%" height="200" />
               </Col>
             </Row>
             <Row>
               <Col style={{ display: "flex" }}>
                 <Col span={7}>
-                  <iframe
-                    src="http://167.71.234.32:3000/public/question/e9b4fb3f-4c30-4e21-86a1-9cfe9ba9e8e0"
-                    frameBorder="0"
-                    width="100%"
-                    height="500"
-                    allowTransparency
-                  ></iframe>
+                <QuestionWithIframe questionId={138} width="100%" height="400" />
                   <div className="Districtwice">
-                    District wise students practising atleast twice a month
+                    District wise students practising atleast once a month
                   </div>
                 </Col>
                 <Col span={7}>
-                  <iframe
-                    src="http://167.71.234.32:3000/public/question/5ada1998-8b3c-48e0-a35b-a882a14c5086"
-                    frameBorder="0"
-                    width="100%"
-                    height="500"
-                    allowTransparency
-                  ></iframe>
+                <QuestionWithIframe questionId={137} width="100%" height="400" />
                   <div style={{ padding: "19.1px" }} className="Districtwice">
                     District wise NIPUN Students
                   </div>
                 </Col>
                 <Col span={10}>
-                  <iframe
-                    src="http://167.71.234.32:3000/public/question/71705c36-c213-47f5-830d-cbb7186fa897"
-                    frameBorder="0"
-                    width="100%"
-                    height="300"
-                    allowTransparency
-                  ></iframe>
+                <QuestionWithIframe questionId={139} width="100%" height="400" />
                   <Button className="navButtonSelected">
                     Class-wise NIPUN Students
                   </Button>
@@ -526,70 +484,9 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                     <div className="District-wise-div">
                       District wise trends
                     </div>
-                    <iframe
-                      src="http://167.71.234.32:3000/public/question/91f6d2a5-dd27-4404-9a78-3e20f46091a6"
-                      frameBorder="0"
-                      width="100%"
-                      height="200"
-                      allowTransparency
-                    ></iframe>
+                    <QuestionWithIframe questionId={143} width="100%" height="200" />
                   </Col>
 
-                  {/* <Col offset={6} span={18}>
-                    <div className="District-wise-div">
-                      District wise trends
-                    </div>
-                    <div>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/91f6d2a5-dd27-4404-9a78-3e20f46091a6"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Top Districts (Class-1)</Button>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/02e65177-2908-4478-82cf-ebe6e6e8719d"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Bottom Districts (Class-1)</Button>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/730d8c5b-e248-4a7f-af24-412ca87042ff"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Top Districts (Class-2)</Button>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/3ec7a9bc-c587-4fb8-ad8f-e4fb3f728cd6"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Bottom Districts (Class-2)</Button>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/39c7afb6-9f14-492b-89ca-39f2a0b13dff"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Top Districts (Class-3)</Button>
-                      <iframe
-                        src="http://167.71.234.32:3000/public/question/8239aab9-23aa-4f60-b661-fbfcde29263a"
-                        frameBorder="0"
-                        width="100%"
-                        height="200"
-                        allowTransparency
-                      ></iframe>
-                      <Button className="navButtonSelected">Bottom Districts (Class-3)</Button>
-                    </div>
-                  </Col> */}
                 </Col>
               </Col>
             </Row>
@@ -613,13 +510,7 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Numeracy Analysis
                 </div>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/5e5d0c47-3259-4b40-a3e1-674464fc3abc"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={140} width="100%" height="180" />
               </Col>
               <Col span={6}>
                 <Button
@@ -628,22 +519,8 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Top Districts (Class-1)
                 </Button>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/02e65177-2908-4478-82cf-ebe6e6e8719d"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={144} width="100%" height="200" />
               </Col>
-
-              {/* <Col span={6}>
-                <div>
-            
-             
-       
-                </div>
-              </Col> */}
               <Col span={9}>
                 <div
                   style={{ padding: "22.6px" }}
@@ -666,13 +543,7 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Numeracy Analysis
                 </div>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/a87d0fac-42e5-4b68-88c1-984ebc12cdd9"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={141} width="100%" height="200" />
               </Col>
               <Col span={6}>
                 <Button
@@ -681,13 +552,7 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Bottom Districts (Class-1)
                 </Button>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/730d8c5b-e248-4a7f-af24-412ca87042ff"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={145} width="100%" height="200" />
               </Col>
 
               <Col span={9}>
@@ -712,13 +577,7 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Numeracy Analysis
                 </div>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/9edfc3fd-d460-4c70-a477-afd5b79b756e"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={142} width="100%" height="200" />
               </Col>
               <Col span={6}>
                 <Button
@@ -727,39 +586,21 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 >
                   Top Districts (Class-2)
                 </Button>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/3ec7a9bc-c587-4fb8-ad8f-e4fb3f728cd6"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={146} width="100%" height="200" />
                 <Button
                   style={{ height: "60px" }}
                   className="navButtonSelected"
                 >
                   Bottom Districts (Class-2)
                 </Button>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/39c7afb6-9f14-492b-89ca-39f2a0b13dff"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={148} width="100%" height="200" />
                 <Button
                   style={{ height: "60px" }}
                   className="navButtonSelected"
                 >
                   Top Districts (Class-3)
                 </Button>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/8239aab9-23aa-4f60-b661-fbfcde29263a"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={149} width="100%" height="200" />
                 <Button
                   style={{ height: "60px" }}
                   className="navButtonSelected"
@@ -768,7 +609,6 @@ const StudentAssessmentPerformanceGrade1_3 = (props: any) => {
                 </Button>
               </Col>
             </Row>
-
           </Col>
         </Row>
       </Content>
