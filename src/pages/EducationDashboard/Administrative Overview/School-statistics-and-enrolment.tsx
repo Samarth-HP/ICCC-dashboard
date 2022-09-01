1abd0b4a91fdc5ba99de07dd39d4b196d8ad7796
Shivansh Rana <shivanshrana789@gmail.com>
2022-09-01 20:05:50 +0530

    embed school-static administrative tab

--- src/pages/EducationDashboard/Administrative Overview/School-statistics-and-enrolment.tsx
+++ src/pages/EducationDashboard/Administrative Overview/School-statistics-and-enrolment.tsx
@@ -17,6 +17,7 @@ import FooterRightLogo from "../../assets/footer_Samarth_Himachal_logo.png";
 import MapComponent from "../../../components/MapComponent/MapComponent";
 import config from "./config.json";
 import API_SERVICE from "../../../services/api-service";
+import QuestionWithIframe from "../../../components/QuestionWIthIframe";
 
 const { Search } = Input;
 
@@ -71,42 +72,18 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
           <Col span={24}>
             <Row>
               <Col span={12}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/5b7445a3-7080-4809-a155-1f80c8ec11d5"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+                <QuestionWithIframe questionId={29} width="100%" height="200" />
               </Col>
               <Col span={12}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/a5f088e7-ab7f-4597-90ed-f08601603341"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+              <QuestionWithIframe questionId={33} width="100%" height="200" />
               </Col>
             </Row>
             <Row>
               <Col span={12}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/d3262f4f-6ff9-445d-8ad6-8011031b4f9d"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+              <QuestionWithIframe questionId={40} width="100%" height="200" />
               </Col>
               <Col span={12}>
-                <iframe
-                  src="http://167.71.234.32:3000/public/question/405d95a9-16c0-4a92-bbcf-3d3f41e1752e"
-                  frameBorder="0"
-                  width="100%"
-                  height="200"
-                  allowTransparency
-                ></iframe>
+              <QuestionWithIframe questionId={34} width="100%" height="200" />
               </Col>
             </Row>
           </Col>
@@ -122,25 +99,13 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
         </Row>
         <Row>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/261ab950-e796-4607-b681-76936092cdc6"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={30} width="100%" height="200" />
             <Button className="navButtonSelected">
               Category Wise Number of Schools
             </Button>
           </Col>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/429feda3-a836-462e-aefe-b48aae083b43"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={32} width="100%" height="200" />
             <Button className="navButtonSelected">
               Session wise Number of Schools
             </Button>
@@ -148,13 +113,7 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
         </Row>
         <Row>
           <Col span={24}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/eb03fa6b-d7e3-45fa-beed-f816d89155dd"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={61} width="100%" height="200" />
             <Button className="navButtonSelected">
               District wise, Category wise, Number of Schools
             </Button>
@@ -162,13 +121,7 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
         </Row>
         <Row>
           <Col span={24}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/8cfd8bb3-a219-4594-b731-e15ca3e064cb"
-              frameBorder="0"
-              width="100%"
-              height="250"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={72} width="100%" height="200" />
             <Button className="navButtonSelected">
               Month-wise Review Meeting Compliance
             </Button>
@@ -181,13 +134,7 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
         </Row>
         <Row>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/8e440682-721e-4f1f-8ff7-0e121bff537f"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={38} width="100%" height="200" />
             <Button className="navButtonSelected">
               {/* Enrolment slab wise school distribution */}
               District-wise Enrolment by Level of Education
@@ -195,13 +142,7 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
           </Col>
 
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/814c83b4-dd3e-45cc-9235-ca7d50437311"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={35} width="100%" height="200" />
             <Button className="navButtonSelected">
               Student Enrolment by Level of Education
             </Button>
@@ -210,45 +151,21 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
 
         <Row style={{ display: "flex" }}>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/2d25967e-479a-4950-a7d5-952c564b9ac7"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={36} width="100%" height="200" />
           </Col>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/fde69400-048e-4fae-ae27-a98d20e14432"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={37} width="100%" height="200" />
           </Col>
         </Row>
         <Row style={{ display: "flex" }}>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/9f041846-f970-452a-a7e0-13c037054954"
-              frameBorder="0"
-              width="100%"
-              height="300"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={43} width="100%" height="300" />
             <Button className="navButtonSelected">
               Pupil-Teacher ratio across level of education
             </Button>
           </Col>
           <Col span={12}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/4ee28061-c30c-436e-b27b-b07de37fecf8"
-              frameBorder="0"
-              width="100%"
-              height="300"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={53} width="100%" height="300" />
             <Button className="navButtonSelected">
               Grade-wise Enrolment distribution
             </Button>
@@ -256,13 +173,7 @@ const SchoolStatisticsAndEnrolment: FC = (props: any) => {
         </Row>
         <Row>
           <Col span={24}>
-            <iframe
-              src="http://167.71.234.32:3000/public/question/0b4eb048-a442-40ad-a214-2e3a03020db3"
-              frameBorder="0"
-              width="100%"
-              height="200"
-              allowTransparency
-            ></iframe>
+          <QuestionWithIframe questionId={54} width="100%" height="200" />
             <Button className="navButtonSelected">
               Stream-wise Enrolment distribution
             </Button>
