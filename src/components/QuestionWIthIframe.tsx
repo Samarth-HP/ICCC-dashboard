ba1c6a353d555e4e779f1a01ee9a26a24002a44f
Keshav Ashta <keshavashta16@gmail.com>
2022-09-01 10:08:05 +0530

    added option for filter in url

--- src/components/QuestionWIthIframe.tsx
+++ src/components/QuestionWIthIframe.tsx
@@ -5,14 +5,17 @@ const jwt = require("jsonwebtoken");
 const METABASE_SITE_URL = "http://167.71.234.32:3000";
 const METABASE_SECRET_KEY = "68a529116afd75d19c1d625133ea50207a6571d5e786a25a24c14f61555886b5";
 
-const QuestionWithIframe = ({questionId, height, width}: { questionId: any, height?: string, width?: string }) => {
+const QuestionWithIframe = ({questionId, height, width, params}: { questionId: any, height?: string, width?: string, params?: any }) => {
     const [url, setUrl] = useState('');
     const generateUrl = () => {
         const payload = {
             resource: {question: questionId},
-            params: {},
+            params: params || {},
             exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
         };
+        if(questionId==75){
+            console.log(payload)
+        }
         const token = jwt.sign(payload, METABASE_SECRET_KEY);
 
         setUrl(METABASE_SITE_URL + "/embed/question/" + token + "#bordered=false&titled=false&downloadable=true");
