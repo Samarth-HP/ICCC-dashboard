b11056e7754c92552175b947659788c51faf8302
Keshav Ashta <keshavashta16@gmail.com>
2022-09-06 16:40:05 +0530

    fixed merge issue question withiframe comp

--- src/components/QuestionWIthIframe.tsx
+++ src/components/QuestionWIthIframe.tsx
@@ -1,43 +1,42 @@
-import React, { useContext, useEffect, useState } from "react";
-import { IframeContextContext } from "../App";
+import React, {useContext, useEffect, useState} from "react";
+import {IframeContextContext} from "../App";
 
 const jwt = require("jsonwebtoken");
 
 const METABASE_SITE_URL = "http://167.71.234.32:3000";
 const METABASE_SECRET_KEY =
-  "68a529116afd75d19c1d625133ea50207a6571d5e786a25a24c14f61555886b5";
+    "68a529116afd75d19c1d625133ea50207a6571d5e786a25a24c14f61555886b5";
 const QuestionWithIframeProtected = ({
-  questionId,
-  height,
-  width,
-  params,
-}: {
+                                       questionId,
+                                       height,
+                                       width,
+                                       nonDownloadable,
+                                       params,
+                                     }: {
   questionId: any;
   height?: string;
   width?: string;
+  nonDownloadable?: boolean;
   params?: any;
 }) => {
-  const { hasFirstIframeLoaded, updateHasFirstIframeLoaded } =
-    useContext(IframeContextContext);
+  const {hasFirstIframeLoaded, updateHasFirstIframeLoaded} =
+      useContext(IframeContextContext);
   const [load, setLoad] = useState(false);
   const [url, setUrl] = useState("");
   const generateUrl = () => {
     const payload = {
-      resource: { question: questionId },
+      resource: {question: questionId},
       params: params || {},
-      exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
+      exp: Math.round(Date.now() / 1000) + 60 * 60 * 24, // 10 minute expiration
     };
-    if (questionId == 75) {
-      // console.log(payload);
-    }
     const token = jwt.sign(payload, METABASE_SECRET_KEY);
-
     setUrl(
-      METABASE_SITE_URL +
+        METABASE_SITE_URL +
         "/embed/question/" +
         token +
-        "#bordered=false&titled=false&downloadable=true"
+        "#bordered=false&titled=false&downloadable=" + !!!nonDownloadable
     );
+
   };
   useEffect(() => {
     if (questionId) {
@@ -47,8 +46,8 @@ const QuestionWithIframeProtected = ({
 
   useEffect(() => {
     if (
-      !hasFirstIframeLoaded &&
-      !localStorage.getItem("hasFirstIframeLoaded")
+        !hasFirstIframeLoaded &&
+        !localStorage.getItem("hasFirstIframeLoaded")
     ) {
       setLoad(true);
     } else if (hasFirstIframeLoaded) {
@@ -57,60 +56,61 @@ const QuestionWithIframeProtected = ({
   }, [hasFirstIframeLoaded]);
   if (!url) {
     return (
-      <div>
-        Not Authenticated, Please check if embed is enabled for this Object
-      </div>
+        <div>
+          Not Authenticated, Please check if embed is enabled for this Object
+        </div>
     );
   }
 
   if (!load) {
     return (
-      <div
-        style={{
-          width: width || "100%",
-          height: height || "300px",
-        }}
-      >
-        Not Loading
-      </div>
+        <div
+            style={{
+              width: width || "100%",
+              height: height || "300px",
+            }}
+        >
+          Not Loading
+        </div>
     );
   }
   return (
-    <iframe
-      src={url}
-      onLoad={() => {
-        setTimeout(() => {
-          updateHasFirstIframeLoaded(true);
-        }, 300);
-      }}
-      style={{ margin: "1px" }}
-      frameBorder="0"
-      width={width || "100%"}
-      height={height || "300px"}
-      allowTransparency
-    />
+      <iframe
+          src={url}
+          onLoad={() => {
+            setTimeout(() => {
+              updateHasFirstIframeLoaded(true);
+            }, 300);
+          }}
+          style={{margin: "1px"}}
+          frameBorder="0"
+          width={width || "100%"}
+          height={height || "300px"}
+          allowTransparency
+      />
   );
 };
 const QuestionWithIframe = ({
-  questionId,
-  height,
-  width,
-  params,
-  nonDownloadable,
-}: {
+                              questionId,
+                              height,
+                              width,
+                              nonDownloadable,
+                              params,
+                            }: {
   questionId: any;
   height?: string;
   width?: string;
+  nonDownloadable?: boolean;
   params?: any;
-  nonDownloadable?: any;
 }) => {
   return (
-    <QuestionWithIframeProtected
-      questionId={questionId}
-      height={height}
-      width={width}
-      params={params}
-    />
+      <QuestionWithIframeProtected
+          questionId={questionId}
+          height={height}
+          width={width}
+          nonDownloadable={nonDownloadable}
+          params={params}
+      />
   );
 };
 export default QuestionWithIframe;
