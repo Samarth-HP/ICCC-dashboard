b471c5e0fdfba065f0f565bb82c043240206763d
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2022-09-07 12:15:07 +0530

    added base url

--- src/services/parameters.ts
+++ src/services/parameters.ts
@@ -2,5 +2,7 @@ declare const API_URL: string;
 export default {
   ApiUrl: "https://iccc.samagra.io/bq-query",
   BaseUrl: "http://iccc.luezoid.com/",
+
+  // use it as base url
   // BaseUrl: "http://localhost:3000/",
 };
