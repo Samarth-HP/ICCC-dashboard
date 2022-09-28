dffb97355d008cc85177f7bed4d28887a8f5640a
Keshav Ashta <keshavashta16@gmail.com>
2022-09-28 10:38:38 +0530

    added new component for samarth dashboard

--- src/pages/DetailedDashBoard/FrameLink.tsx
+++ src/pages/DetailedDashBoard/FrameLink.tsx
@@ -18,6 +18,7 @@ import FooterRightLogo from "../../assets/footer_Samarth_Himachal_logo.png";
 import Login_Img from "../../assets/Login_Img.png";
 import Side_Img from "../../assets/image 135.png";
 import { useHistory } from "react-router-dom";
+import SamarthQuestionWithIframe from "../../components/SamarthQuestionWIthIframe";
 
 const IframeLink = (showLink: any) => {
   const {
@@ -42,11 +43,14 @@ const IframeLink = (showLink: any) => {
     );
   } else if (params.name === "Enrolment") {
     return (
-      <iframe
-        src="https://samarthhp-metabase.in/public/dashboard/e08ae997-520a-42a7-99e8-96a9483546e7"
-        width="100%"
-        height="100%"
-      ></iframe>
+
+        <SamarthQuestionWithIframe
+            questionId={883}
+            width="100%"
+            height="100%"
+              params={{'district':['CHAMBA']}}
+
+              />
     );
   } else if (params.name === "StudentAttendance") {
     return (
